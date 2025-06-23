7d8288131461051071f9716bb66abae19e95ea15
Charles042 <ekechukwuma20@gmail.com>
2025-06-23 23:43:22 +0100

    -fix: fixed the navbar

---
+++ components/shared-components/components/menu-bar.tsx
@@ -0,0 +1,10 @@
+const   MenuBar = () => {
+  return(
+    <div className="flex flex-col gap-2">
+        <span className="w-5 h-0.5 bg-black"></span>
+        <span className="w-6 h-0.5 bg-black"></span>
+    </div>
+  )
+};
+
+export default MenuBar;
--- components/shared-components/naviagtion-bar.tsx
+++ components/shared-components/naviagtion-bar.tsx
@@ -4,12 +4,17 @@ import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { usePathname } from "next/navigation";
+import MenuBar from "./components/menu-bar";
+import { useState } from "react";
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger, DrawerClose, DrawerFooter } from "@/components/ui/drawer";
+import { RiCloseLargeFill } from "react-icons/ri";
 
 
 
 const NavigationBar = () => {
 
     const pathname = usePathname();
+    const [isOpen, setIsOpen] = useState(false);
     const navItems = [
         {
             label: "Works",
@@ -30,7 +35,13 @@ const NavigationBar = () => {
   return (
     <nav className="flex justify-between items-center max-w-[1400px] mx-auto py-5">
       <div className="mx-5 lg:mx-0">
-        <Image src="/umaanidi.png" alt="logo" width={100} height={100} className="max-sm:w-20 "/>
+        <Image
+          src="/umaanidi.png"
+          alt="logo"
+          width={100}
+          height={100}
+          className="max-sm:w-20 "
+        />
       </div>
       <div className="hidden lg:block">
         <ul className="flex gap-4 bg-[#F4F4F4] py-2 px-2 rounded-full">
@@ -55,6 +66,34 @@ const NavigationBar = () => {
         </Link>
         <FaLinkedin className="text-xl" />
         <MdEmail className="text-xl" />
+      </div>
+      <div className="lg:hidden mx-5">
+        <Drawer direction="right">
+          <DrawerTrigger>
+            <MenuBar />
+          </DrawerTrigger>
+          <DrawerContent className=" p-5">
+            <DrawerTitle className="text-center text-2xl font-bold hidden">
+              Menu
+            </DrawerTitle>
+            <DrawerClose className="flex justify-end">
+              <RiCloseLargeFill className="text-xl text-[#2f7d96]" />
+            </DrawerClose>
+            <div className="flex flex-col justify-center items-center my-auto gap-10 ">
+              {navItems.map((item) => (
+                <Link
+                  key={item.label}
+                  href={item.href}
+                  className={`hover:bg-[#F4F4F4] w-full text-center p-3 rounded-lg text-[#2f7d96] hover:text-[#56CCF2] ${
+                    pathname === item.href ? "bg-[#F4F4F4] text-[#56CCF2] font-bold" : ""
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </DrawerContent>
+        </Drawer>
       </div>
     </nav>
   );
